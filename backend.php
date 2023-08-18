0c4afebde1bf6f00c86e8c9996d3ffac4cb0e6f2
bishoDev123 <bisholiveenglish@gmail.com>
2023-08-18 12:53:20 +0300

    Merge branch 'main' of https://github.com/bishoDev123/HR-learning-app

--- backend.php
+++ backend.php
@@ -1,6 +1,13 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = htmlspecialchars($_POST["username"]);
 
-    echo $username;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $errorMessage = "";
+
+    if (empty($username)) {
+        header("Location: index.php");
+        $errorMessage = "fields cannot be empty";
+    }
 }
+?>
